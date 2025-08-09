f3b3b75ebe2c1ec70bb0d7ecba86437b0b51ac5f
Alex Jones <alexkjones3700@gmail.com>
2025-08-09 09:46:16 -0400

    I added a stack class. This is a test for pushing changes and does not reflect what the main file of the project will look like.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 
-
+from StackClass import Stack 
 if __name__ == "__main__":
     print("Hello, World!")
+    nums = Stack()
+    nums.push(1)
+    nums.push(2)
+    nums.push(3)
+    print(nums.peek())
+    nums.pop()
+    print(nums.peek())
+    nums.pop()
+    print(nums.peek())
 
